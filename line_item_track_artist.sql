711840d3ef541376278fe727bb1b555b64235191
daniel salgado <daniel.salgado.p@gmail.com>
2022-08-07 20:15:06 +0200

    showing Artist name (not Composer)

--- line_item_track_artist.sql
+++ line_item_track_artist.sql
@@ -1,4 +1,7 @@
 --13) line_item_track_artist.sql: Provide a query that includes the purchased track name AND artist name with each invoice line item.
- SELECT t.Name, t.Composer, i.InvoiceLineId
-  FROM [InvoiceLine] i 
-  JOIN Track t ON i.TrackId = t.TrackId;
+SELECT  t.Name, ar.Name, i.InvoiceId
+FROM InvoiceLine i
+         JOIN Track t on i.TrackId = t.TrackId
+         JOIN Album a on T.AlbumId = a.AlbumId
+         JOIN Artist ar on a.ArtistId = ar.ArtistId
+;
