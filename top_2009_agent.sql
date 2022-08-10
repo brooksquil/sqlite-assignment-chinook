4e55e21f72ac49e85a8ec6a1bba42ff705e3e292
daniel salgado wsdl <daniel.salgado.p@gmail.com>
2022-08-10 16:33:01 +0200

    groping by Id (not Name)

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -2,11 +2,11 @@
 SELECT Employee.FirstName, Employee.LastName,
 ROUND(SUM(Invoice.Total), 2) as 'Total Sales' 
 FROM Employee 
-JOIN Customer, Invoice 
+JOIN Customer, Invoice
 WHERE Customer.SupportRepId = Employee.EmployeeId  
 AND Employee.Title = 'Sales Support Agent' 
 AND Customer.CustomerId = Invoice.CustomerId 
 AND Invoice.InvoiceDate 
 LIKE '2009%' 
-GROUP BY Employee.FirstName 
+GROUP BY Employee.EmployeeId
 ORDER BY (round(sum(Invoice.Total), 2))  DESC LIMIT 1;
